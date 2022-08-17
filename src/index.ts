6102e46ac96033be0a890fb1e383b91b20df189e
akr7777 <dimitrius77@protonmail.com>
2022-08-17 17:02:41 +0300

    second3

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import {categoriesAPI, materialsAPI} from "./trashData/materials";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const jsonBodyMiddleware = express.json();
 app.use(jsonBodyMiddleware);
