c02bb87820f3148b63d71b725316f60d681af283
akr7777 <dimitrius77@protonmail.com>
2022-08-20 14:28:36 +0300

    trying to connect to db

--- src/controllers/materials.controller.ts
+++ src/controllers/materials.controller.ts
@@ -3,7 +3,6 @@ const db = require('./../DataBaseAPI/db');
 class MaterialsController {
     async getMaterials(req: any, res: any) {
         console.log('materialsController/getMaterials BEFORE the query')
-        //const allMaterials = await db.query(`SELECT * FROM materials;`);
         const allMaterials = db.query1('Select * from materials;')
         console.log('materialsController/getMaterials AFTER the query, allMaterials=',allMaterials)
         res.json(allMaterials.rows);
