d1b29d5bcc6805cea95bdd0fac12db6d40102c02
akr7777 <dimitrius77@protonmail.com>
2022-08-21 14:44:59 +0300

    210822-1

---
+++ dist/trashData/categories.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.categoriesAPI = void 0;
+exports.categoriesAPI = [
+    { id: 1, parentId: null, label: "Матeматика", items: [] },
+    { id: 2, parentId: 1, label: "алгебра", items: [] },
+    { id: 3, parentId: 1, label: "rеометрия", items: [] },
+    { id: 4, parentId: 3, label: "круги", items: [] },
+    { id: '5', parentId: 3, label: "квадраты", items: [] },
+    { id: 6, parentId: null, label: "Физика", items: [] },
+];
