5aa208dd5fe71f6fdd57c0383cfb721a90cdd6d5
akr7777 <dimitrius77@protonmail.com>
2022-08-26 00:08:19 +0300

    db test

--- dist/DataBaseAPI/db.js
+++ dist/DataBaseAPI/db.js
@@ -42,18 +42,43 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let pg = require("pg");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    host: '195.133.196.81',
+    database: 'public',
+    port: 5432,
+    user: 'postgres',
+    password: '111111',
+    /*connection: {
+        //connectionString: '',//process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    },
+    ssl: false,*/
+};
+const pg_1 = __importDefault(require("pg"));
+//let pg = require("pg")
 let connectionString = "pg://postgres:postgres@localhost:5432/students";
 connectionString = 'postgres://vdbadvhizpjein:8ffdc7f70733091e511e36537270d8f6c291c60cc4154823c2e0955223bf8aa0@ec2-44-208-88-195.compute-1.amazonaws.com:5432/dc9ba0jlsur7fn';
-let client = new pg.Client(connectionString);
+let client = new pg_1.default.Client(process.env.DATABASE_URL);
 //client.connect();
 module.exports = {
     query1: (text) => __awaiter(void 0, void 0, void 0, function* () {
         //const client = await new Client(config);
-        console.log('Trying to connect...');
-        yield client.connect();
-        console.log('connected');
-        const res = yield client.query(text);
-        console.log('res', res);
+        try {
+            console.log('!!!!!!!!Trying to connect.66..');
+            yield client.connect();
+            console.log('!!!!!!!!connected');
+            const res = yield client.query(text);
+            console.log('res', res);
+            return res;
+        }
+        catch (e) {
+            console.log('312432error=', e);
+        }
     })
 };
--- src/DataBaseAPI/db.ts
+++ src/DataBaseAPI/db.ts
@@ -33,20 +33,40 @@ const config = {
 }
 */
 
-let pg = require("pg")
+const config = {
+    host: '195.133.196.81',
+    database: 'public',
+    port: 5432,
+    user: 'postgres',
+    password: '111111',
+    /*connection: {
+        //connectionString: '',//process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    },
+    ssl: false,*/
+}
+import pg from 'pg';
+//let pg = require("pg")
 let connectionString = "pg://postgres:postgres@localhost:5432/students";
 connectionString = 'postgres://vdbadvhizpjein:8ffdc7f70733091e511e36537270d8f6c291c60cc4154823c2e0955223bf8aa0@ec2-44-208-88-195.compute-1.amazonaws.com:5432/dc9ba0jlsur7fn';
-let client = new pg.Client(connectionString);
+let client = new pg.Client(process.env.DATABASE_URL);
 //client.connect();
 
 module.exports = {
     query1: async (text: string) => {
         //const client = await new Client(config);
-
-        console.log('Trying to connect...');
-        await client.connect();
-        console.log('connected');
-        const res = await client.query(text);
-        console.log('res', res)
+        try {
+            console.log('!!!!!!!!Trying to connect.66..');
+            await client.connect();
+            console.log('!!!!!!!!connected');
+            const res = await client.query(text);
+            console.log('res', res);
+            return res;
+        }
+        catch (e) {
+            console.log('312432error=', e)
+        }
     }
 }
