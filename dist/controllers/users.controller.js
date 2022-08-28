f6b826c49466080329c58ff488ad2bbdc32d37e4
akr7777 <dimitrius77@protonmail.com>
2022-08-28 15:12:41 +0300

    tring to add login function test 2

--- dist/controllers/users.controller.js
+++ dist/controllers/users.controller.js
@@ -19,25 +19,30 @@ class UsersController {
             console.log('UsersController / req=', req);
             const email = req;
             const password = req;
-            const SQL = 'SELECT * FROM USERS WHERE email="";';
-            let client = new pg_1.default.Client(process.env.DATABASE_URL);
-            yield client.connect();
-            const credentials = yield client.query(SQL);
-            const response = {
-                id: '',
-                name: '',
-                email: '',
-                isAdmin: false
-            };
-            if (credentials.rows.length === 1) {
-                /*response.id = credentials[0].id;
-                response.name = credentials[0].name;
-                response.email = credentials[0].email;
-                response.isAdmin = credentials[0].isAdmin;*/
-                console.log('UsersController / credentials = ', credentials);
+            try {
+                const SQL = 'SELECT * FROM USERS WHERE email="";';
+                let client = new pg_1.default.Client(process.env.DATABASE_URL);
+                yield client.connect();
+                const credentials = yield client.query(SQL);
+                const response = {
+                    id: '',
+                    name: '',
+                    email: '',
+                    isAdmin: false
+                };
+                if (credentials.rows.length === 1) {
+                    /*response.id = credentials[0].id;
+                    response.name = credentials[0].name;
+                    response.email = credentials[0].email;
+                    response.isAdmin = credentials[0].isAdmin;*/
+                    console.log('UsersController / credentials = ', credentials);
+                }
+                res.status(200).json(credentials.rows);
+                yield client.end();
             }
-            res.status(200).json(credentials.rows);
-            yield client.end();
+            catch (e) {
+                console.log('!!!!!erorr=!!!!', e);
+            }
         });
     }
     getUser(req, res) {
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -7,26 +7,30 @@ class UsersController {
         const email = req;
         const password = req;
 
-        const SQL = 'SELECT * FROM USERS WHERE email="";'
-        let client = new pg.Client(process.env.DATABASE_URL);
-        await client.connect();
-        const credentials = await client.query(SQL);
+        try {
+            const SQL = 'SELECT * FROM USERS WHERE email="";'
+            let client = new pg.Client(process.env.DATABASE_URL);
+            await client.connect();
+            const credentials = await client.query(SQL);
 
-        const response = {
-            id: '',
-            name: '',
-            email: '',
-            isAdmin: false
+            const response = {
+                id: '',
+                name: '',
+                email: '',
+                isAdmin: false
+            }
+            if (credentials.rows.length === 1) {
+                /*response.id = credentials[0].id;
+                response.name = credentials[0].name;
+                response.email = credentials[0].email;
+                response.isAdmin = credentials[0].isAdmin;*/
+                console.log('UsersController / credentials = ', credentials)
+            }
+            res.status(200).json(credentials.rows);
+            await client.end();
+        } catch (e) {
+            console.log('!!!!!erorr=!!!!', e)
         }
-        if (credentials.rows.length === 1) {
-            /*response.id = credentials[0].id;
-            response.name = credentials[0].name;
-            response.email = credentials[0].email;
-            response.isAdmin = credentials[0].isAdmin;*/
-            console.log('UsersController / credentials = ', credentials)
-        }
-        res.status(200).json(credentials.rows);
-        await client.end();
     }
 
     async getUser (req: any, res: any) {
