3cfa6855cdbf1a988dfb0fc7ff23bfd3148ad884
akr7777 <dimitrius77@protonmail.com>
2022-08-28 23:15:39 +0300

    try to do description db

--- src/rotes/description.routes.ts
+++ src/rotes/description.routes.ts
@@ -6,5 +6,6 @@ const descriptionController = require('./../controllers/description.controller')
 //router.post('/setDescription', descriptionController.login);
 router.get('/getDescription', descriptionController.getDescription);
 //router.get('/getAllCategories', tasksController.getAllCategories);
+//sfdjh
 
 module.exports = router;
