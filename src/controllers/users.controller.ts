8033d660ff80c7881fc6d0174eac82b7b2602f7f
akr7777 <dimitrius77@protonmail.com>
2022-08-30 16:10:50 +0300

    try to do avatar file uploads

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -39,7 +39,8 @@ class UsersController {
         try {
             console.log('userController / avatarUpload, req=', req)
             //req.files.photo.mv('public/uploads/'+req.files.photo.name);
-            const {file, id} = req.body; //body={ file={} id='002'}
+            const {id } = req.body; //body={ file={} id='002'}
+            const file = req.files.file;
             //res.end(req.files.photo.name);
             //console.log(req.files.photo); // the uploaded file object
             console.log('userController / avatarUpload, file=', file, 'id=', id);
