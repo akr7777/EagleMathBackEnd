ef04e464f995c1216a11dc07466ecaef569de940
akr7777 <dimitrius77@protonmail.com>
2022-08-30 13:50:35 +0300

    try to do avatar file uploads

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -36,10 +36,14 @@ class UsersController {
     }
 
     async avatarUpload (req: any, res: any) {
-        console.log('userController / avatarUpload, req.files=', req.files)
-        req.files.photo.mv('public/uploads/'+req.files.photo.name);
-        res.end(req.files.photo.name);
-        console.log(req.files.photo); // the uploaded file object
+        try {
+            console.log('userController / avatarUpload, req=', req)
+            req.files.photo.mv('public/uploads/'+req.files.photo.name);
+            res.end(req.files.photo.name);
+            console.log(req.files.photo); // the uploaded file object
+        } catch (e) {
+            console.log('!!!usersController, avatarUpload, error = ', e)
+        }
     }
 }
 
