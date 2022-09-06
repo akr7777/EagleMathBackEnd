cc9a29dd2b5894638428e83ff4e46f1029bcf63c
akr7777 <dimitrius77@protonmail.com>
2022-09-06 17:31:25 +0300

    Trying to do token authorization 333

--- src/rotes/users.routes.ts
+++ src/rotes/users.routes.ts
@@ -4,8 +4,8 @@ export const router = new Router();
 const usersController = require('./../controllers/users.controller');
 
 router.post('/login', usersController.login);
-router.post('/token', usersController.token);
-router.post('/logout', usersController.logout);
+//router.post('/token', usersController.token);
+//router.post('/logout', usersController.logout);
 
 router.get('/getuser', usersController.getUser);
 router.post('/uploadAvatar', usersController.avatarUpload);
