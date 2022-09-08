5f869e40d9a379ff2b700ef20775229381d9ff46
akr7777 <dimitrius77@protonmail.com>
2022-09-09 01:09:17 +0300

    Trying to do token authorization. looks all works

--- dist/rotes/contacts.routes.js
+++ dist/rotes/contacts.routes.js
@@ -4,6 +4,7 @@ exports.router = void 0;
 const Router = require("express");
 exports.router = new Router();
 const contactsController = require('./../controllers/contacts.controller');
-exports.router.get('/getContacts', contactsController.getContacts);
+const authMiddleware = require('../middlewares/auth-middleware');
+exports.router.get('/getContacts', authMiddleware, contactsController.getContacts);
 exports.router.post('/setContacts', contactsController.setContacts);
 module.exports = exports.router;
--- src/rotes/contacts.routes.ts
+++ src/rotes/contacts.routes.ts
@@ -2,8 +2,9 @@ const Router = require("express");
 export const router = new Router();
 
 const contactsController = require('./../controllers/contacts.controller');
+const authMiddleware = require('../middlewares/auth-middleware');
 
-router.get('/getContacts', contactsController.getContacts);
+router.get('/getContacts', authMiddleware, contactsController.getContacts);
 router.post('/setContacts', contactsController.setContacts);
 
 module.exports = router;
